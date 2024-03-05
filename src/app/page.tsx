cfdb4da3d3ce26311bfbaf754a2a7d5bd73b75fc
adrianmcenturion <adrianmcenturion@gmail.com>
2024-03-05 15:55:41 -0300

    fix auth in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import {redirect} from "next/navigation";
 
 import readUserSession from "@/lib/actions";
-import {publicPaths} from "@/lib/utils";
+import {privatePaths, publicPaths} from "@/lib/utils";
 
 export default async function Home() {
   const {data} = await readUserSession();
 
-  if (!data.session) {
-    return redirect(publicPaths.login);
-  }
+  !data.session ? redirect(publicPaths.login) : redirect(privatePaths.dashboard);
 
   return (
     <h2 className="text-center">Necesitas iniciar sesión para poder ver el calendario de pagos.</h2>
